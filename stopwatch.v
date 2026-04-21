6f7368a71d0b2375946def6a5c33b3405f35b982
CarsonRasmussen <carsonrasmussen@u.boisestate.edu>
2026-04-21 17:17:37 -0600

    Fixes

--- stopwatch.v
+++ stopwatch.v
@@ -1,10 +1,10 @@
 //StopWatch: Modulo-60 Counter
 module stopwatch(
-    input Enable,
-    input Clock,
-    input Reset,
-    output Out,
-    output [5:0] State //6-bits to represent the highest number 59
+    input en,
+    input clk,
+    input rst,
+    output out,
+    output [5:0] state //6-bits to represent the highest number 59
 );
     
     wire [5:0] Connect;
@@ -19,67 +19,67 @@ module stopwatch(
     wire CountOut;
     wire OutD;
     
-    assign CountOut = (State[2] & ~State[1] & State[0]) | Reset;
-    assign OutD = Out ^ CountOut;     
+    assign CountOut = (state[2] & ~state[1] & state[0]) | rst;
+    assign OutD = out ^ CountOut;     
 
     dff dff1(
         .D(DConnect[0]),
-        .Clock(Clock),
+        .Clock(clk),
         .Reset(CountOut),
-        .Q(State[0]),
+        .Q(state[0]),
         .NotQ(Connect[0])
     );
     
     dff dff2(
         .D(DConnect[1]),
-        .Clock(Clock),
+        .Clock(clk),
         .Reset(CountOut),
-        .Q(State[1]),
+        .Q(state[1]),
         .NotQ(Connect[1])
     );
 
     dff dff3(
         .D(DConnect[2]),
-        .Clock(Clock),
+        .Clock(clk),
         .Reset(CountOut),
-        .Q(State[2]),
+        .Q(state[2]),
         .NotQ(Connect[2])
     );
 
     dff dff4(
         .D(DConnect[3]),
-        .Clock(Clock),
+        .Clock(clk),
         .Reset(CountOut),
-        .Q(State[3]),
+        .Q(state[3]),
         .NotQ(Connect[3])
     );
     
     dff dff5(
         .D(DConnect[4]),
-        .Clock(Clock),
+        .Clock(clk),
         .Reset(CountOut),
-        .Q(State[4]),
+        .Q(state[4]),
         .NotQ(Connect[4])
     );
 
     dff dff6(
         .D(DConnect[5]),
-        .Clock(Clock),
+        .Clock(clk),
         .Reset(CountOut),
-        .Q(State[5),
+        .Q(state[5),
         .NotQ(Connect[5])
     );
     
     dff dffout(
         .D(OutD),
-        .Clock(Clock),
-        .Reset(Reset),
-        .Q(Out),
+        .Clock(clk),
+        .Reset(rst),
+        .Q(out),
         .NotQ(ConnectOut)
     );
     
     full_adder adder1(
-        .A(State[0]),
+        .A(state[0]),
         .B(1'b1),
         .Cin(1'b0),
         .Cout(Connect1),
@@ -87,7 +87,7 @@ module stopwatch(
     );
     
     full_adder adder2(
-        .A(State[1]),
+        .A(state[1]),
         .B(1'b0),
         .Cin(Connect1),
         .Cout(Connect2),
@@ -95,7 +95,7 @@ module stopwatch(
     );
 
     full_adder adder3(
-        .A(State[2]),
+        .A(state[2]),
         .B(1'b0),
         .Cin(Connect2),
         .Cout(Connect3),
@@ -103,7 +103,7 @@ module stopwatch(
     );
 
     full_adder adder4(
-        .A(State[3]),
+        .A(state[3]),
         .B(1'b0),
         .Cin(Connect3),
         .Cout(Connect4),
@@ -111,7 +111,7 @@ module stopwatch(
     );
 
     full_adder adder5(
-        .A(State[4]),
+        .A(state[4]),
         .B(1'b0),
         .Cin(Connect4),
         .Cout(Connect5),
@@ -119,7 +119,7 @@ module stopwatch(
     );
 
      full_adder adder6(
-        .A(State[5]),
+        .A(state[5]),
         .B(1'b0),
         .Cin(Connect5),
         .Cout(Connect6),
