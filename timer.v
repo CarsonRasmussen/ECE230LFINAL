fec6f7cc3edb07364782bb8f0903de159b61a4e5
JonibekU <Jonibekutaev@u.boisestate.edu>
2026-04-22 01:50:52 -0600

    done

--- timer.v
+++ timer.v
@@ -8,6 +8,24 @@ module timer(
     output [5:0] state     //6-bits to represent the highest number 59
 );
 
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        state <= 6'd0;  // makes the state = 0 in other words resets the timmer
+    end
+    else if (load) begin
+        state <= load_value; // strores/sets the state to the load_value
+    end
+    else if (en) begin                 // if enabled the counter de-increments by 1 and stops when it hits 0.
+        if (state != 6'd0)
+            state <= state - 1; 
+        else
+            state <= 6'd0; // stay at 0
+    end
+    else begin
+        state <= state; // hold when en is 0 and load is 0 it doesnt change. bascially pauses the timer.
+    end
+end
+
 
 
 endmodule
